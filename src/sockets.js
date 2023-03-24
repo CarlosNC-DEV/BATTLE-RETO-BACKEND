99c7a45e40eae7290194ab961dea59f6511c6c9e
CarlosNC-DEV <carlosnogueradev@gmail.com>
2023-03-24 16:12:54 -0500

    Sala Sigo

--- src/sockets.js
+++ src/sockets.js
@@ -5,32 +5,31 @@ export default (io)=>{
         console.log(`Usuario conectado: ${socket.id}`);
 
         socket.on('crear-juego', () => {
-            const codigo = Math.random().toString(36).substring(2, 8).toUpperCase();
-            juegos[codigo] = [socket.id];
+          const codigo = Math.random().toString(36).substring(2, 8).toUpperCase();
+          juegos[codigo] = [];
+          socket.emit('juego-creado', codigo);
+        }); 
+    
+        socket.on('unirse-a-juego', (codigo) => {
+          if (juegos[codigo] && juegos[codigo].length < 7) {
+            juegos[codigo].push(socket.id);
             socket.join(codigo);
-            socket.emit('juego-creado', codigo);
+            const miPosicion = juegos[codigo].indexOf(socket.id);
+            io.to(codigo).emit('jugadores-actualizados', juegos[codigo].length);
+            socket.emit('unido-a-juego', { jugadores: juegos[codigo], posicion: miPosicion });
+            socket.codigoJuego = codigo;
+          } else {
+            socket.emit('codigo-invalido');
+          }
         });
         
-        socket.on('unirse-a-juego', (codigo) => {
-            if (juegos[codigo] && juegos[codigo].length < 7) {
-              juegos[codigo].push(socket.id);
-              socket.join(codigo);
-              io.to(codigo).emit('jugadores-actualizados', juegos[codigo].length);
-              socket.emit('unido-a-juego');
-              socket.codigoJuego = codigo;
-            } else {
-              socket.emit('codigo-invalido');
-            }
-        });
-  
-
         socket.on('disconnect', () => {
-            console.log('a player disconnected');
-            const codigo = socket.codigoJuego;
-            if (codigo && juegos[codigo]) {
-              juegos[codigo] = juegos[codigo].filter((id) => id !== socket.id);
-              io.to(codigo).emit('jugadores-actualizados', juegos[codigo].length);
-            }
+          console.log('a player disconnected');
+          const codigo = socket.codigoJuego;
+          if (codigo && juegos[codigo]) {
+            juegos[codigo] = juegos[codigo].filter((id) => id !== socket.id);
+            io.to(codigo).emit('jugadores-actualizados', juegos[codigo].length);
+          }
         });
     });
     
